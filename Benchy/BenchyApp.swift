5baef8ec4c77db108de0cdbf23306889d56457e1
Christian Flanders <christianflanders@northwesternmutual.com>
2024-02-29 11:10:59 -0600

    Initial Commit

---
+++ Benchy/BenchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchyApp.swift
+//  Benchy
+//
+//  Created by Flanders, Christian on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BenchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
